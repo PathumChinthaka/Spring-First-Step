6c2b15fe3cdf46ffbc8acca97d6247b3c4e7ac02
pathum-chinthaka <104442327+pathum-chinthaka@users.noreply.github.com>
2023-08-19 12:22:47 +0530

    multi value mapping

--- src/main/java/com/example/springstartup/api/Employee.java
+++ src/main/java/com/example/springstartup/api/Employee.java
@@ -1,11 +1,11 @@
 package com.example.springstartup.api;
 
+import org.springframework.http.MediaType;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
+import java.util.Arrays;
 import java.util.Map;
 
 @RestController
@@ -16,5 +16,17 @@ public class Employee {
     public String saveEmplyee(String id, String name, @RequestParam MultiValueMap<String,String> empData){
         System.out.println(empData);
         return String.format("EmpDetails(%s,%s)",id,name);
+    }
+
+    @PostMapping
+    public String getMultiTypeData(String id,String country,String city,
+        @RequestParam("towns") String[] towns,@RequestParam MultiValueMap<String,String>data){
+        System.out.println(id);
+        System.out.println(country);
+        System.out.println(city);
+        System.out.println(Arrays.toString(towns));
+        System.out.println(data);
+
+        return "saved";
     }
 }
